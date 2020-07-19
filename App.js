d1ed625f348cd787427888149b753890ca41e008
최성준 <csj2702@naver.com>
2020-07-19 17:10:51 +0900

    최초 생성

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  Text
+} from 'react-native';
+
+function App() {
+  return (
+    <>
+    <Text>
+      Keep Study
+    </Text>
+    </>
+  );
+};
+
+
+export default App;
